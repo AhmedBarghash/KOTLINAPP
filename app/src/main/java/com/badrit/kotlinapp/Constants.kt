b51bb3754484f4590bb960c30371b817778d1482
ahmedosamabadrit <ahmed.osama@badrit.com>
2017-09-19 15:01:40 +0200

    adding the Constants file using companion object

---
+++ app/src/main/java/com/badrit/kotlinapp/Constants.kt
@@ -0,0 +1,11 @@
+package com.badrit.kotlinapp
+
+/**
+ * Created by ahmed-osama on 19/09/17.
+ */
+class Constants {
+    companion object {
+        val NUMBER_OF_USERS: Int = 100
+        val BASE_URL :String = "https://randomuser.me/"
+    }
+}
