5ba55a3a0fcf436f0ff1109df2c881d99beb87f0
ahmedosamabadrit <ahmed.osama@badrit.com>
2017-09-20 10:52:38 +0200

    change  the type of the array list string into Result class handle the view of the viewHolder

--- app/src/main/java/com/badrit/kotlinapp/adapter/MyAdapter.kt
+++ app/src/main/java/com/badrit/kotlinapp/adapter/MyAdapter.kt
@@ -5,20 +5,30 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.badrit.kotlinapp.R
+import com.badrit.kotlinapp.pojos.Result
 import com.badrit.kotlinapp.ui.view_holder.ViewHolder
+import com.bumptech.glide.Glide
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+
+
 
 
 /**
  * Created by ahmed-osama on 17/09/17.
  */
-class MyAdapter (var mydata: ArrayList<String>?, val mContext:Context): RecyclerView.Adapter<ViewHolder>() {
+class MyAdapter (var mydata: ArrayList<Result>?, val mContext:Context): RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(mContext).inflate(R.layout.rowitem, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.userName?.text = mydata?.get(position)
+        holder?.userName?.text = "${mydata?.get(position)?.name?.title}  ${mydata?.get(position)?.name?.first}  ${mydata?.get(position)?.name?.last}"
+        holder?.userEmai?.text = mydata?.get(position)?.email
+        Glide.with(mContext)
+                .load(mydata?.get(position)?.picture?.medium)
+                .bitmapTransform(RoundedCornersTransformation(mContext, 200, 0, RoundedCornersTransformation.CornerType.ALL))
+                .into(holder?.userImage)
     }
 
     override fun getItemCount(): Int {
